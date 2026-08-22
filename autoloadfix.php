f68a291a3caf4b66a5c1538056b01bcd64550f4c
rejoyanislam9009-lab <rejoyanislam9009@gmail.com>
2026-08-22 21:45:06 +0300

    Remove unused translation domain path

--- autoloadfix.php
+++ autoloadfix.php
@@ -10,7 +10,6 @@
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       autoloadfix
- * Domain Path:       /languages
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
